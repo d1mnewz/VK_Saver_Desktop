e458786c2d26dcf379f0804f210a63eae6f03c8d
ZhluktenkoDmytro <d1mnewz@gmail.com>
2016-11-17 15:45:17 +0200

    init commit with simple auth

---
+++ VK_Saver_Desktop/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace VK_Saver_Desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginBox = new System.Windows.Forms.TextBox();
+            this.PassBox = new System.Windows.Forms.TextBox();
+            this.LogLabel = new System.Windows.Forms.Label();
+            this.PassLabel = new System.Windows.Forms.Label();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LoginBox
+            // 
+            this.LoginBox.Location = new System.Drawing.Point(90, 46);
+            this.LoginBox.Name = "LoginBox";
+            this.LoginBox.Size = new System.Drawing.Size(153, 20);
+            this.LoginBox.TabIndex = 0;
+            // 
+            // PassBox
+            // 
+            this.PassBox.Location = new System.Drawing.Point(90, 72);
+            this.PassBox.Name = "PassBox";
+            this.PassBox.PasswordChar = '*';
+            this.PassBox.Size = new System.Drawing.Size(153, 20);
+            this.PassBox.TabIndex = 1;
+            // 
+            // LogLabel
+            // 
+            this.LogLabel.AutoSize = true;
+            this.LogLabel.Location = new System.Drawing.Point(12, 53);
+            this.LogLabel.Name = "LogLabel";
+            this.LogLabel.Size = new System.Drawing.Size(33, 13);
+            this.LogLabel.TabIndex = 2;
+            this.LogLabel.Text = "Login";
+            // 
+            // PassLabel
+            // 
+            this.PassLabel.AutoSize = true;
+            this.PassLabel.Location = new System.Drawing.Point(12, 75);
+            this.PassLabel.Name = "PassLabel";
+            this.PassLabel.Size = new System.Drawing.Size(53, 13);
+            this.PassLabel.TabIndex = 3;
+            this.PassLabel.Text = "Password";
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(90, 98);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(75, 23);
+            this.SubmitButton.TabIndex = 4;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.PassLabel);
+            this.Controls.Add(this.LogLabel);
+            this.Controls.Add(this.PassBox);
+            this.Controls.Add(this.LoginBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LoginBox;
+        private System.Windows.Forms.TextBox PassBox;
+        private System.Windows.Forms.Label LogLabel;
+        private System.Windows.Forms.Label PassLabel;
+        private System.Windows.Forms.Button SubmitButton;
+    }
+}
+
---
+++ VK_Saver_Desktop/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VkNet;
+namespace VK_Saver_Desktop
+{
+    public partial class Form1 : Form
+    {
+        public const int appId = 5733434;
+        VkApi api;
+
+        public Form1()
+        {
+            InitializeComponent();
+            api = new VkApi();
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            //api.Authorize(appId, LoginBox.Text, this.PassBox.Text, VkNet.Enums.Filters.Settings.All); // авторизуемся
+            api.Authorize(new ApiAuthParams
+            {
+                ApplicationId = appId,
+                Login = LoginBox.Text,
+                Password = PassBox.Text,
+                Settings = VkNet.Enums.Filters.Settings.All
+            });
+            MessageBox.Show(api.UserId.ToString());
+        }
+    }
+}
